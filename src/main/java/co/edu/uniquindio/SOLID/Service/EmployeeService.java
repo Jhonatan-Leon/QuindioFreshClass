709dd8dca6863ae8f35855a56ad981c2e8a9cdac
Jhonatan-Leon <c.jhonatan2007@gmail.com>
2025-10-31 09:45:40 -0500

    refactor: Update EmployeeService methods to return EmpleadoDTO
    - Changed all method return types from `Empleado` to `EmpleadoDTO` for consistency with DTO usage.
    - Added `getAll` method to retrieve the first employee as DTO.

--- src/main/java/co/edu/uniquindio/SOLID/Service/EmployeeService.java
+++ src/main/java/co/edu/uniquindio/SOLID/Service/EmployeeService.java
@@ -27,13 +27,17 @@ public class EmployeeService {
         }
     }
 
-    public Empleado createEmployee(EmpleadoDTO newEmployee) {
+    public EmpleadoDTO getAll(){
+        return minimercado.getEmpleados().stream().findFirst().map(EmpleadoMapper::toDTO).orElse(null);
+    }
+
+    public EmpleadoDTO createEmployee(EmpleadoDTO newEmployee) {
         if(searchEmployee(newEmployee.getId()) != null){
             throw new IllegalArgumentException("Ya existe un empleado con ese ID");
         }
         Empleado emp = EmpleadoMapper.toEntity(newEmployee);
         minimercado.addEmpleado(emp);
-        return emp;
+        return EmpleadoMapper.toDTO(emp);
     }
 
 
@@ -44,7 +48,7 @@ public class EmployeeService {
         return null;
     }
 
-    public Empleado updateEmployee(EmpleadoDTO employee) {
+    public EmpleadoDTO updateEmployee(EmpleadoDTO employee) {
         Empleado e = searchEmployee(employee.getId());
 
         if (e == null) {
@@ -54,7 +58,7 @@ public class EmployeeService {
         if (employee.getNombre() != null) e.setNombre(employee.getNombre());
         if (employee.getRol() != null) e.setRol(employee.getRol());
         if (false) {if (employee.getActivo()) e.activar();else e.inactivar(); }
-        return e;
+        return EmpleadoMapper.toDTO(e);
 
     }
 
@@ -66,7 +70,7 @@ public class EmployeeService {
         minimercado.eliminarEmpleado(e);
     }
 
-    public Empleado lockEmployee(String id,boolean state){
+    public EmpleadoDTO lockEmployee(String id,boolean state){
         Empleado e = searchEmployee(id);
         if(e == null){
             throw new IllegalArgumentException("No existe un empleado con ese ID");
@@ -76,10 +80,10 @@ public class EmployeeService {
         }else{
             e.inactivar();
         }
-        return e;
+        return EmpleadoMapper.toDTO(e);
     }
 
-    public Empleado activateEmployee(String id, boolean state){
+    public EmpleadoDTO activateEmployee(String id, boolean state){
         Empleado e = searchEmployee(id);
         if(e == null){
             throw new IllegalArgumentException("No existe un empleado con ese ID");
@@ -89,7 +93,7 @@ public class EmployeeService {
         }else{
             e.inactivar();
         }
-        return e;
+        return EmpleadoMapper.toDTO(e);
     }
 
 
