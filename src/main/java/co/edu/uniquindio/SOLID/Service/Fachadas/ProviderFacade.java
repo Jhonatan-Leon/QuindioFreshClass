a3b02e8ff5e82b4cbc1f618efc5c81f896a37d06
Jhonatan-Leon <c.jhonatan2007@gmail.com>
2025-10-31 01:06:59 -0500

    feat: Add methods to ProviderFacade for managing providers
    - Implemented singleton pattern for ProviderFacade.
    - Added methods for provider creation, update, deletion, and state management.
    - Integrated with ProviderService to handle provider operations.

--- src/main/java/co/edu/uniquindio/SOLID/Service/Fachadas/ProviderFacade.java
+++ src/main/java/co/edu/uniquindio/SOLID/Service/Fachadas/ProviderFacade.java
@@ -1,5 +1,53 @@
 package co.edu.uniquindio.SOLID.Service.Fachadas;
 
+import co.edu.uniquindio.SOLID.Model.DTO.ProveedorDTO;
+import co.edu.uniquindio.SOLID.Model.Proveedor;
+import co.edu.uniquindio.SOLID.Service.ProviderService;
+
+import java.util.List;
+
 public class ProviderFacade {
+    private static ProviderFacade Instance;
+    private final ProviderService providerService;
+
+    public static ProviderFacade getInstance(){
+        if(Instance == null){
+            Instance = new ProviderFacade();
+        }
+        return Instance;
+    }
+
+    private ProviderFacade(){
+        this.providerService = ProviderService.getInstance();
+    }
+
+    public List<Proveedor> getAllProviders(){
+        return providerService.getAllProvider();
+    }
+
+    public Proveedor CreateProvider(Proveedor newProvider){
+       Proveedor provider = providerService.createProvider(newProvider);
+       return provider;
+    }
+
+    public Proveedor searchProvider(){
+        return providerService.searchProvider();
+    }
+
+    public Proveedor updateProvider(ProveedorDTO provider){
+        return providerService.updateProvider(provider);
+    }
+
+    public void deleteProvider(String id){
+        providerService.deleteProvider(id);
+    }
+
+    public Proveedor lockProvider(String nit,boolean state){
+        return providerService.lockProvider(nit,state);
+    }
+
+    public Proveedor activateProvider(String nit, boolean state){
+        return providerService.activateProvider(nit, state);
+    }
 
 }
